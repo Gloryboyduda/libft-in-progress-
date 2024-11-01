15c1d7c69a9304468c74477f00ad00cd852e40d6
duandrad <duandrad@student.42lisboa.com>
2024-11-01 22:58:15 +0000

    strnstr memchr memcmp

--- ft_memchr.c
+++ ft_memchr.c
@@ -13,7 +13,27 @@
 #include "libft.h"
 #include <string.h>
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	
+	size_t			i;
+	unsigned char	*source;
+
+	i = 0;
+	source = (unsigned char *) s;
+	if (*source == '\0' || n == 0)
+		return (NULL);
+	while (i < n)
+	{
+		if (source[i] == (unsigned char) c)
+			return ((void *)(source + i));
+		i++;
+	}
+	return (NULL);
 }
+/* int main()
+{
+	char s[] = "Hello";
+	printf("%p\n", memchr(s, 'o', 5));
+	printf("%p", ft_memchr(s, 'o', 5));
+	return 0;
+} */
---
+++ ft_strnstr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: duandrad <duandrad@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 18:43:28 by duandrad          #+#    #+#             */
+/*   Updated: 2024/11/01 18:43:28 by duandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	if (to_find[0] == '\0')
+		return ((char *) str);
+	while (str[i] && i < len)
+	{
+		j = 0;
+		while (str[i + j] == to_find[j] && i + j < len)
+		{
+			if (to_find[j + 1] == '\0')
+				return ((char *)(str + i));
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/* int main()
+{
+	char string[] = "Hello World";
+	char to_find[] = "W";
+	printf("%s", ft_strnstr(string, to_find, 3));
+} */
