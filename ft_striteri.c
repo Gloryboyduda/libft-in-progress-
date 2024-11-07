d4d46e1aede85a5c4b90dd551015cfc20605fe2b
duandrad <duandrad@student.42lisboa.com>
2024-11-07 13:04:21 +0000

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -12,4 +12,27 @@
 
 #include "libft.h"
 
-void ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, s);
+		i++;
+	}
+}
+/* 
+void f(unsigned int n, char *str)
+{
+	while (*str)
+		*str + n;
+}
+
+int main()
+{
+	char *test = "hello";
+	ft_striteri(test, f);
+	printf("%s", test);
+} */
